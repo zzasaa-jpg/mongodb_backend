ae0a0fb4240ab2eb036e0ea33681fb7bce7b80d7
[Mahendra Kumar] <[zzsdrt354@gmail.com]>
2024-11-26 17:03:32 +0530

    CURD operations Implemented.

--- main.js
+++ main.js
@@ -18,6 +18,10 @@ mongoose.connect('mongodb+srv://zzsdrt354:SrQzx3GgKGM9cer@cluster0.fktok.mongodb
     useUnifiedTopology: true,
 });
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "connection error"));
+db.once('open', ()=> console.log("connected!"))
+
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     password: { type: String, required: true },
@@ -55,9 +59,32 @@ app.get('/', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/form.html'))
+    res.sendFile(path.join(__dirname, 'public/form.html'));
+});
+// --------------------------------------------------------------
+app.get('/edit', async (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public/edit.html'));
+});
+
+// READ (GET)
+app.get('/Users', async (req, res)=>{
+    const data = await User.find()
+    res.send(data)
+
+});
+
+// Update (PUT)
+app.put('/Users/:id', async(req, res)=>{
+    const updateData = await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    res.send(updateData);
 })
 
+// Delete (DELETE)
+app.delete('/Users/:id', async(req, res)=>{
+    await User.findByIdAndDelete(req.params.id);
+    res.send({Message: "deleted Succefully!"})
+})
+//--------------------------------------------------------------------
 app.listen(port, () => {
     console.log(`running${port}`)
 })
